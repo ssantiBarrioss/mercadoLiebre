4a1c85a5f6becc94e0e59ddc94282d209904613b
ssantiBarrioss <santiago.barrios@hotmail.es>
2023-10-20 01:05:45 -0300

    subo archivos

---
+++ app.js
@@ -0,0 +1,14 @@
+const express=require("express")
+const app= express()
+const path=require("path")
+const port= 3000
+
+app.get("/",(req, res)=>{
+    res.sendFile(path.join(__dirname, "/views/home.html"))
+})
+
+app.use(express.static("public"))
+
+app.listen(port,()=>{
+    console.log(`Levanto servidor en puerto: ${port}`)
+})
