6a2165a681451a29c995d406642c47c751e6b8f3
ssantiBarrioss <santiago.barrios@hotmail.es>
2023-11-09 21:08:04 -0300

    subo formularios

--- app.js
+++ app.js
@@ -2,9 +2,29 @@ const express=require("express")
 const app= express()
 const path=require("path")
 const port= 3000
-
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.get("/",(req, res)=>{
     res.sendFile(path.join(__dirname, "/views/home.html"))
+})
+
+app.get("/register", (req, res)=>{
+    res.sendFile(path.join(__dirname, "/views/register.html"))
+})
+    
+app.post("/register", (req, res)=>{
+    console.log(req.body)
+    res.redirect("/login")
+})
+
+app.get("/login", (req, res)=>{
+    res.sendFile(path.join(__dirname, "/views/login.html"))
+})
+
+app.post("/login", (req,res)=>{
+    console.log(req.body);
+    res.redirect("/")
 })
 
 app.use(express.static("public"))
